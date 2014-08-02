50a2cf4a47d1dc35b2da572fb9a9e114fa219279
Casey Falk <caseyfalk94@gmail.com>
2014-08-01 21:33:03 -0400

    Cleaned up some more code, added HTML-escaping, and began to add HTML RegEx.

--- marky.js
+++ marky.js
@@ -5,7 +5,8 @@ var lexer = {
   "multilineComment": {
       "javascript": "/\\*"+any+"*?\\*/",
       "python": "\"\"\""+any+"*?\"\"\"",
-      "ruby":"__END__"+any+"*|\\=begin"+any+"*?\\=end"
+      "ruby":"__END__"+any+"*|\\=begin"+any+"*?\\=end",
+      "html":"\\<!--"+any+"*--\\>"
   },
 
   "inlineComment": {
@@ -34,7 +35,8 @@ var lexer = {
   "syntax": {
       "javascript": ["[","]","{","}","(",")",";", ",", ".", ":"],
       "python": ["[","]","{","}","(",")",";", ",", ".", ":"],
-      "ruby": ["[","]","{","}","(",")",";", ",", ".", ":"]
+      "ruby": ["[","]","{","}","(",")",";", ",", ".", ":"],
+      "html": ["<",">"]
   },
 
   "keyword": {
@@ -59,7 +61,21 @@ var lexer = {
                "def", "do", "else", "elsif", "end", "ensure", "for", "if", "in",
                "module", "next", "nil", "puts", "print", "redo", "rescue",
                "retry", "return", "self", "super", "then", "undef", "unless",
-               "until", "when", "while", "yield", "__FILE__", "__LINE__"]
+               "until", "when", "while", "yield", "__FILE__", "__LINE__"],
+      "html": ["a", "abbrev", "acronym", "address", "applet", "area", "au",
+                "author", "b", "banner", "base", "basefont", "bgsound", "big",
+                "blink", "blockquote", "bq", "body", "br", "caption", "center",
+                "cite", "code", "col", "colgroup", "credit", "del", "dfn", "dir",
+                "div", "dl", "dt", "dd", "em", "embed", "fig", "fn", "font", "form",
+                "frame", "frameset", "h1", "h2", "h3", "h4", "h5", "h6", "head",
+                "hr", "html", "i", "iframe", "img", "input", "ins", "isindex",
+                "kbd", "lang", "lh", "li", "link", "listing", "map", "marquee",
+                "math", "menu", "meta", "multicol", "nobr", "noframes", "note",
+                "ol", "overlay", "p", "param", "person", "plaintext", "pre", "q",
+                "range", "samp", "script", "select", "small", "spacer", "spot",
+                "strike", "strong", "sub", "sup", "tab", "table", "tbody", "td",
+                "textarea", "textflow", "tfoot", "th", "thead", "title", "tr",
+                "tt", "u", "ul", "var", "wbr", "xmp"]
   },
 
   "bool": {
@@ -93,13 +109,16 @@ var precedence = {  // Earliest === Highest Precedence
 
 var borders = {
   //"javascript": { ... }, //Example
+  "html": {
+    //"keyword":"(?:\<\s*\/?\s*)" //TODO
+  },
   "default": {
     "keyword":"(?:[^a-zA-Z]|^|$)",
     "inlineComment":"(?:[^\"']|^|$)",
   }
 }
 
-var languages = ["javascript", "python", "ruby"];
+var languages = ["javascript", "python", "ruby", "html"];
 
 
 function loadRegExpArray(arr) {
@@ -208,9 +227,6 @@ function measureMatchQuality(text, matches) {
 }
 
 
-// # # # # # # # # # # # # # # # # # # # # # #
-
-
 function markyInferLanguage(text) {
   //TODO: test QUALITY instead of just QUANTITY.
   //TODO: IE: Get the amount of unmatched text.
@@ -242,111 +258,115 @@ function markyGetMatches(text, language) {
   //Apply each of the language token filters to add the appropriate classes.
   $.each(lexer, function(token, languageOptions){
     var regexContent = languageOptions[language];
-    // Escape any characters in array inputs that need to be escaped.
-    if (regexContent instanceof Array) {
-      regexContent = loadRegExpArray(regexContent);
+    if (regexContent !== undefined) {
+      // Escape any characters in array inputs that need to be escaped.
+      if (regexContent instanceof Array) {
+        regexContent = loadRegExpArray(regexContent);
+      }
+
+
+      // Borders indicate characters that can not be adjacent to a token.
+      // Border Priority: language-specific, a cross-language default, then none.
+      var border = borders[language];
+      if (border===undefined) border = borders["default"];
+      border = border[token]
+      if (border===undefined) border = "";
+
+      var pattern = new RegExp(border+"("+regexContent+")"+border, "gm");
+
+      var match;
+      while (match=pattern.exec(text)) {
+        var matchedText = match[1];
+        var regexStart = match["index"];
+        var start = text.slice(regexStart).indexOf(matchedText)+regexStart;
+        var matchObj = {
+          "start":start,
+          "end":start+matchedText.length,
+          "text":matchedText,
+          "token":token,
+          "escape":language==="html"
+        };
+        matchObjs.push(matchObj);
+      }
     }
-
-
-    // Borders indicate characters that can not be adjacent to a token.
-    // Border Priority: language-specific, a cross-language default, then none.
-    var border = borders[language];
-    if (border===undefined) border = borders["default"];
-    border = border[token]
-    if (border===undefined) border = "";
-
-    var pattern = new RegExp(border+"("+regexContent+")"+border, "gm");
-
-    var match;
-    while (match=pattern.exec(text)) {
-      var matchedText = match[1];
-      var regexStart = match["index"];
-      var start = text.slice(regexStart).indexOf(matchedText)+regexStart;
-      var matchObj = {
-        "start":start,
-        "end":start+matchedText.length,
-        "text":matchedText,
-        "token":token
-      };
-      matchObjs.push(matchObj);
-    }
-
   });
 
   //Sort and filter the matches to avoid double-matching.
   var matchObjs = filterMatches(matchObjs, language)
-  console.log(language)
-  console.log(matchObjs);
   return matchObjs;
+}
 
+function escapeHTML(string) {
+  var pre = document.createElement("pre");
+  var text = document.createTextNode(string);
+  pre.appendChild(text);
+  return pre.innerHTML;
+}
+
+function applyMarkyWrapper(match, text) {
+  var wrapperOpen = "<span class='marky-"+match["token"]+"'>";
+  var wrapperClose = "</span>";
+
+  var textParts = match["text"].split("\n");
+
+  var newText = "";
+  for (var i=0; i<textParts.length; i++) {
+    var content = textParts[i];
+    if (match["escape"]===true) content = escapeHTML(textParts[i]);
+
+    var newLine = (textParts.length-1>i) ? "_!SPACER!_" : "";
+    newText += wrapperOpen + content + wrapperClose + newLine
+  }
+
+  return text.slice(0, match["start"]) +
+                    newText.replace(/_!SPACER!_/g,"\n") +
+                    text.slice(match["end"], text.length);
+}
+
+function buildMarkyCodeSection(lines) {
+  var lineContent ="<div class='lineContainer'>"
+  var lineNumbers = "<div class='lineNumberContainer unselectable'>";
+
+  for (var i=0; i<lines.length; i++){
+    var line = lines[i];
+
+    if (line.trim()!=="") {
+      lineContent += "<div class='codeLine'>" +
+                   "<span class='line'>" +
+                    line +
+                   "</span>" +
+                  "</div>";
+      lineNumbers += "<div class='lineNumber'>"+(i+1)+"</div>";
+    } else {
+      lineContent += "<div class='codeLine emptyCodeLine'></div>";
+      lineNumbers += "<div class='emptyLineNumber lineNumber'>"+(i+1)+"</div>";
+    }
+  }
+
+  lineContent += "</div>";
+  lineNumbers += "</div>";
+
+  return lineNumbers + lineContent;
 }
 
 function markyText(text, language) {
   //Convert tabs to 4 spaces for cross-browser display continuity.
-  var formattedText = text.replace(/\t/g, "    ")
+  var text = text.replace(/\t/g, "    ")
 
-  var matches = markyGetMatches(formattedText, language);
+  var matches = markyGetMatches(text, language);
 
   for (var i=0; i < matches.length; i++) {
-    var match = matches[i];
-
-    var wrapperOpen = "<span class='marky-"+match["token"]+"'>";
-    var wrapperClose = "</span>";
-
-    var textParts = match["text"].split("\n");
-
-    var newText = "";
-    for (var j=0; j<textParts.length; j++) {
-      var newLine = (textParts.length-1>j) ? "_!SPACER!_" : "";
-      newText += wrapperOpen + textParts[j] + wrapperClose + newLine
-    }
-    formattedText = formattedText.slice(0, match["start"]) +
-                      newText +
-                      formattedText.slice(match["end"], formattedText.length);
+    text = applyMarkyWrapper(matches[i], text)
   }
-
 
   //Format the text in lines for the HTML container.
-  var rawLines =  formattedText.replace(/^\n*/,"").replace(/\n*$/,"")
-                               .replace(/_!SPACER!_/g,"\n")
-                               .split("\n");
-
-  var emptyLines = {};
-  var lines ="<div class='lineContainer'>"
-  for (var i=0; i<rawLines.length; i++){
-    var lineContent = rawLines[i];
-
-    if (lineContent.trim()!=="") {
-      lines += "<div class='codeLine'>" +
-                      "<span class='lineContent'>" +
-                      lineContent +
-                      "</span>" +
-                      "</div>";
-    } else {
-      emptyLines[i] = true;
-      lines += "<div class='codeLine emptyCodeLine'></div>";
-    }
-  }
-  lines += "</div>";
-
-  //And apply an equal number of .lineNumber elements.
-  var lineNumbers = "<div class='lineNumberContainer unselectable'>";
-  for (var i=0; i<rawLines.length; i++){
-    if (emptyLines[i]===undefined){
-      lineNumbers += "<div class='lineNumber'>"+(i+1)+"</div>";
-    } else {
-      lineNumbers += "<div class='emptyLineNumber lineNumber'>"+(i+1)+"</div>";
-    }
-  }
-  lineNumbers += "</div>";
-
-
-  return lineNumbers + lines;
+  var lines =  text.split("\n");
+  return buildMarkyCodeSection(lines)
 }
 
-
 function markySection($codeSection) {
-  var text = $codeSection.text().trim();
+  //Escape any inner HTML content.
+  var text = $codeSection.html().trim();
 
   // If the lang isn't given, attempt to infer what it should be.
   var lang = $codeSection.attr("language");
